abcdae67406524ba09ad2ca9ba6c4eee3be6ffff
nguyensonhai <nguyensonhai1009@gmail.com>
2020-05-29 20:11:43 +0700

    update

--- app/EzBooking/app/src/main/java/com/eztech/search/MainActivity.java
+++ app/EzBooking/app/src/main/java/com/eztech/search/MainActivity.java
@@ -32,8 +32,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        refactor();
         initialization();
+        refactor();
 
         //events
         txtNhap.addTextChangedListener(new TextWatcher() {
